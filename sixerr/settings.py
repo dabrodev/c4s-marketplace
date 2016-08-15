375c9ec1bfa3aafa712d22e852ee648747e75f56
Damian Dabrowski <dabdamian@gmail.com>
2016-08-15 20:17:56 +0200

    Heroku

--- sixerr/settings.py
+++ sixerr/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# To serve static files on Heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 AUTHENTICATION_BACKENDS = (
     'social.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend',
@@ -133,3 +136,8 @@ LOGIN_REDIRECT_URL = '/'
 
 SOCIAL_AUTH_FACEBOOK_KEY = '891635960981399'
 SOCIAL_AUTH_FACEBOOK_SECRET = 'd7fe30120f32ba8ed945054dbdc39388'
+
+# Replace database setting to use postgresql on Heroku
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
